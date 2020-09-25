b3d3709389d40c109eb498324a1d4b81841d3468
chic-state-0895 <brookeabryski@gmail.com>
2020-09-25 17:54:01 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -51,7 +51,7 @@ puts "#{idx}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
 
  def list_artists
 Artist.all.sort_by(&:name).each.with_index(1) do |artist, idx|
-puts "#{idx}. #{song.artist.name}"
+puts "#{idx}. #{artist.name}"
 end
 end
 
