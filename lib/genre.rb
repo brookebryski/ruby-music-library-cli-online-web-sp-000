9ea2d78cb8ced754cf650308d79e3ce7994d3fe5
chic-state-0895 <brookeabryski@gmail.com>
2020-09-25 17:51:03 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,46 +1,4 @@
-=begin
-class Genre
-  extend Concerns::Findable
-  attr_accessor :name, :songs
 
-  @@all = []
-
-  def initialize(name)
-  @name = name
-  @songs = []
-  save
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.destroy_all
-    @@all = []
-  end
-
-  def save
-    @@all << self
-  end
-
-  def self.create(name)
-    self.new(name)
-  end
-
-  def artists
-     @artist_array = []
-     @songs.each do |song|
-       if @artist_array.include?(song.artist)
-         nil
-       else
-         @artist_array << song.artist
-       end
-     end
-     @artist_array
-   end
-
-end
-=end
 class Genre
   extend Concerns::Findable
   attr_accessor :name
